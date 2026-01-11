cbbf9a6e397a549a17dee2ff255d46b8b32c4a42
Hohashvili May <hohashvilima@mta.ac.il>
2026-01-11 23:12:49 +0200

    Initial solution with 3 projects

---
+++ Interfaces.Menus04.Ex/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Menus04.Ex
+{
+    public class Class1
+    {
+    }
+}
